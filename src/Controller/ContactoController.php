648353bae79502d52c1b4b718a115bdd6ab719e7
LUPUS001 <antoniodaw23@gmail.com>
2025-09-25 08:14:00 +0000

    Subo mi proyecto Symfony

---
+++ src/Controller/ContactoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactoController extends AbstractController
+{
+    private $contactos = [
+        1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juan@ieselcaminas.org"],
+        2 => ["nombre" => "Ana López", "telefono" =>  "58958448", "email" => "anita@ieselcaminas.org"],
+        5 => ["nombre" => "Mario Montero", "telefono" => "5326824", "email" => "mario.mont@ieselcaminas.org"],
+        7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm200@ieselcaminas.org"],
+        9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
+    ];
+
+    //Para que se muestre el código 1 por defecto:
+    //#[Route('/contacto/{codigo?1}', name: 'ficha_contacto')]
+    //Sin que salga nada por defecto:
+    //#[Route('/contacto/{codigo}', name: 'ficha_contacto')]
+
+    #[Route('/contacto/{codigo?1}', name: 'ficha_contacto')]
+    public function ficha($codigo): Response
+    {
+        $resultado = ($this->contactos[$codigo] ?? null);
+
+        if ($resultado) {
+            $html = "<ul>";
+                $html .= "<li>" . $codigo . "</li>";
+                $html .= "<li>" . $resultado['nombre']  . "</li>";
+                $html .= "<li>" . $resultado['telefono'] . "</li>";
+                $html .= "<li>" . $resultado['email'] . "</li>";
+            $html .= "</ul>";
+            return new Response("<html><body>$html</body>");
+        }
+        return new Response("<html><body>Contacto $codigo no encontrado</body>");
+
+    }
+}
