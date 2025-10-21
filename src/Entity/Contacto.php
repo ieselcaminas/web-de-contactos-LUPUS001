46535d0f030853da858973512f6773b751ec76ab
LUPUS001 <antoniodaw23@gmail.com>
2025-10-21 08:38:41 +0000

    Commit de generación de formularios

--- src/Entity/Contacto.php
+++ src/Entity/Contacto.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactoRepository::class)]
 class Contacto
@@ -14,16 +15,20 @@ class Contacto
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $nombre = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $telefono = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type:"string", length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email(message:"El email {{ value }} no es válido")]
     private ?string $email = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\ManyToOne(inversedBy: 'contactos')]
+    #[Assert\NotBlank]
     private ?Provincia $provincia = null;
 
     
@@ -37,7 +42,7 @@ class Contacto
         return $this->nombre;
     }
 
-    public function setNombre(string $nombre): static
+    public function setNombre(string $nombre): self
     {
         $this->nombre = $nombre;
 
@@ -49,7 +54,7 @@ class Contacto
         return $this->telefono;
     }
 
-    public function setTelefono(string $telefono): static
+    public function setTelefono(string $telefono): self
     {
         $this->telefono = $telefono;
 
@@ -61,7 +66,7 @@ class Contacto
         return $this->email;
     }
 
-    public function setEmail(string $email): static
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
@@ -78,7 +83,7 @@ class Contacto
         return $this->provincia;
     }
 
-    public function setProvincia(?Provincia $provincia): static
+    public function setProvincia(?Provincia $provincia): self
     {
         $this->provincia = $provincia;
 
